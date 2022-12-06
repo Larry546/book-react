b33be3f6d84e091b0f9200fca812d957d2148d6a
Haonan Lu <hnlu546@gmail.com>
2022-12-06 15:08:46 -0500

    change

--- src/services/config.js
+++ src/services/config.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-export const API_URL = "http://ec2-34-228-111-190.compute-1.amazonaws.com:4000";
+export const API_URL = process.env.API_URL || "http://localhost:4000";
 // "http://localhost:4000"
-// "https://book-server.onrender.com"
 // "http://ec2-34-228-111-190.compute-1.amazonaws.com:4000"
 
 export const api = axios.create({withCredentials: true});
