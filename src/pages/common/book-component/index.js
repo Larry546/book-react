b56a746b6fd5f0e56483f26988210d9871674ee6
Haonan Lu <hnlu546@gmail.com>
2022-12-07 19:11:30 -0500

    book-component

--- src/pages/common/book-component/index.js
+++ src/pages/common/book-component/index.js
@@ -5,14 +5,14 @@ const BookComponent = ({info}) => {
       <div className="col-lg-6 col-md-12">
         <Link to={`/book/${info.isbn13}`} className="text-decoration-none text-black d-flex flex-row">
           <img className="rounded" height={200} src={info.image} alt={"cover"}/>
-          <div className="d-flex flex-column">
-            <p className="fw-bold m-3">{info.title}</p>
-            <p className="ms-3">{info.subtitle}</p>
-            {info.authors && <p className="ms-3">Authors: {info.authors}</p>}
-            <p className="mt-auto ms-3">
+          <div className="d-flex flex-column mt-3 justify-content-around">
+            <p className="fw-bold">{info.title}</p>
+            <p>{info.subtitle}</p>
+            {info.authors && <p>Authors: {info.authors}</p>}
+            {info.rating && <p>
               <i className="bi bi-star-fill text-warning"></i>
-              <span className="ms-2">4.0</span>
-            </p>
+              <span className="ms-2">{info.rating}</span>
+            </p>}
           </div>
         </Link>
       </div>
